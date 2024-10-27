1c240b6b5cce8f975bd2de425b794e9cde435224
Kbs1902 <kbsvinay12345@gmail.com>
2024-10-27 23:22:13 +0530

    Add files via upload

---
+++ Employee Management System/Employee.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
+    }
+}
